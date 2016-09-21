7f4cf39077332ca8973bacca97728db6d72fc25c
Audrey Kang <hkang10@gmail.com>
2016-09-21 15:25:33 -0500

    add comment to questions

---
+++ dances_with_wolves/app/controllers/comments.rb
@@ -0,0 +1,16 @@
+get '/questions/:id/comments/new' do
+  @question = Question.find(params[:id])
+  erb :"/comments/_new_question_comment", layout: false, locals: { question: @question }
+end
+
+post '/questions/:id/comments' do
+comment = Comment.new(text: params[:text], commenter_id: session[:user_id], commentable_id: params[:id], commentable_type: 'Question')
+question = Question.find(params[:id])
+
+  if comment.save
+    redirect "/questions/#{question.id}"
+  else
+    @errors = comment.errors.full_messages
+    erb :'/questions/show'
+  end
+end
