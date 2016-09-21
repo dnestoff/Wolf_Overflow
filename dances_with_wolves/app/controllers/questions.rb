0f7c3013d58fa573024d04d001f2b1b468f892e8
Audrey Kang <hkang10@gmail.com>
2016-09-21 13:39:59 -0500

    add error partial

--- dances_with_wolves/app/controllers/questions.rb
+++ dances_with_wolves/app/controllers/questions.rb
@@ -4,15 +4,20 @@ get '/questions' do
   erb :"/questions/index"
 end
 
+post '/questions' do
+  @question = Question.new(title: params[:title], text: params[:text])
+
+  if @question.save
+    redirect '/questions'
+  else
+    @errors = ["input cannot be empty"]
+    redirect "/questions/#{@question.id}"
+  end
+end
+
 get '/questions/:id' do
   @question = Question.find(params[:id])
   @answers = @question.answers
 
   erb :"/questions/show"
-end
-
-post '/questions' do
-  @question = Question.create(title: params[:title], text: params[:text])
-
-  redirect "/questions"
 end
