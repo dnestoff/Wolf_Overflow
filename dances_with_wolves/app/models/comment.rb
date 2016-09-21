8d268ca0e72aa75809c07e59e109cada5bd2dd4e
Mark McLoughlin <email": null>
2016-09-21 16:22:41 -0500

    Add voting to comments on questions- TODO: fix bug affecting comment-on-question vote count

--- dances_with_wolves/app/models/comment.rb
+++ dances_with_wolves/app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :commenter, class_name: 'User'
   belongs_to :commentable, polymorphic: true
+  has_many :votes, as: :voteable
 
   validates :text, presence: true
 end
