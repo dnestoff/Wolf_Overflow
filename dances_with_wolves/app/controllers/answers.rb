e2b5f3633cf2b0a5ab28928fad70144c0ce8c430
dnestoff <email": null>
2016-09-21 11:44:41 -0500

    Add other routes for putting and deleting and delete answers

--- dances_with_wolves/app/controllers/answers.rb
+++ dances_with_wolves/app/controllers/answers.rb
@@ -1,9 +1,15 @@
-
-
-# get '/questions/:id/answers/:id' do
-
-# end
+get '/question/:id/answers/new' do
+  #essentially for javascript not being used
+end
 
 post '/question/:id/answers' do
+
+end
+
+put '/question/:id/answers/:id' do
+
+end
+
+delete '/question/:id/answers/:id' do
 
 end
