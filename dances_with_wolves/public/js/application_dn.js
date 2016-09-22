2ca75fa1e2fc33c599a3df0b0dd5f58ea3bae5eb
dnestoff <email": null>
2016-09-22 16:02:53 -0500

    Create ajax calls and on-page voting functions for buttons on answers/questions/comments

--- dances_with_wolves/public/js/application_dn.js
+++ dances_with_wolves/public/js/application_dn.js
@@ -1,5 +1,50 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
+  upvoteImageUrlBlack = 'https://image.freepik.com/free-icon/thumbs-up-hand-outline_318-41813.jpg'
+  downvoteImageUrlBlack = 'https://image.freepik.com/free-icon/thumbs-down-hand-outline_318-41753.png'
+  upvoteImageUrlRed = 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQQNuq1aPO7gTLJQhWKzYhY2AsxhLcrIgnEwjzdYczepdzuZeztnQ';
+  downvoteImageUrlRed = 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQnhEA0mRKeAP1RCA-oXzrzobsL0ERbtHs1m-BgjGSUPleO_hSRypJnTeg';
+
+  $(".upvote-button").submit( function(event) {
+    event.preventDefault();
+    $form = $(this);
+    $image = $form.find('input[type="image"]');
+
+    request = $.ajax({
+      method: $form.attr("method"),
+      url: $form.attr("action")
+    });
+
+    request.done( function(response) {
+      $voteTotal = $form.closest("tr").find(".vote-total").text(response.votes);
+      console.log("Vote has been added");
+      if (response.vote_id != null) {
+        $image.attr('src', upvoteImageUrlRed);
+      } else {
+        $image.attr('src', upvoteImageUrlBlack);
+      };
+    });
+  });
+
+  $(".downvote-button").submit( function(event) {
+    event.preventDefault();
+    $form = $(this);
+    $image = $form.find('input[type="image"]');
+
+    request = $.ajax({
+      method: $form.attr("method"),
+      url: $form.attr("action")
+    });
+
+    request.done( function(response) {
+      $voteTotal = $form.closest("tr").find(".vote-total").text(response.votes);
+      console.log("Vote has been added");
+      if (response.vote_id != null) {
+        $image.attr('src', downvoteImageUrlRed);
+      } else {
+        $image.attr('src', downvoteImageUrlBlack);
+      };
+    });
+  });
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
