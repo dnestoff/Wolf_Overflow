0c7cc8f62499b086d21e046d4f84b20c62ec798b
Mark McLoughlin <email": null>
2016-09-22 11:24:28 -0500

    Complete user profile page

--- dances_with_wolves/app/controllers/users.rb
+++ dances_with_wolves/app/controllers/users.rb
@@ -1,17 +1,53 @@
 get '/users/:id' do
   if logged_in?
-    p "LOGGGGGGGGGGED IN"
-    p current_user_id
-    p params[:id]
     if current_user_id == params[:id].to_i
-      " USER MATCHES"
       @user = logged_in_user
       erb :'user/show'
     else
-      p "@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-      redirect "/users/#{params[:id]}"
+      redirect "/users/#{current_user_id}"
     end
   else
     redirect '/'
   end
+end
+
+get '/users/:id/questions' do
+  if logged_in?
+    if current_user_id == params[:id].to_i
+      @questions = User.find(params[:id]).questions
+      erb :'questions/index'
+    else
+      redirect "/users/#{current_user_id}/questions"
+    end
+  else
+    redirect '/'
+  end
+end
+
+get '/users/:id/answers' do
+  if logged_in?
+    if current_user_id == params[:id].to_i
+      @answers = User.find(params[:id]).answers
+      erb :'answers/index'
+    else
+      redirect "/users/#{current_user_id}/answers"
+    end
+  else
+    redirect '/'
+  end
+
+end
+
+get '/users/:id/comments' do
+  if logged_in?
+    if current_user_id == params[:id].to_i
+      @comments = User.find(params[:id]).comments
+      erb :'comments/index'
+    else
+      redirect "/users/#{current_user_id}/comments"
+    end
+  else
+    redirect '/'
+  end
+
 end
