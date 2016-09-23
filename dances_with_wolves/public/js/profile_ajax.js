3166fa26bcd2eeaa125c14ad677ce188833561e0
Mark McLoughlin <mark.mcloughlin1@gmail.com>
2016-09-23 14:03:20 -0500

    Finish ajax on profile page; style profile page

--- dances_with_wolves/public/js/profile_ajax.js
+++ dances_with_wolves/public/js/profile_ajax.js
@@ -9,11 +9,50 @@ $(document).ready(function(){
 
     .done(function(response) {
       $('#user-questions-table').html(response);
+      $('#question-expand').hide();
     })
     .fail(function() {
       console.log("error");
     })
 
 
-  })
-})
+  });
+  $('#profile-page-container').on('click','#answer-expand', function(event){
+    event.preventDefault();
+    console.log($(this).attr('href'));
+
+    $.ajax({
+      url: $(this).attr('href'),
+    })
+
+    .done(function(response) {
+      $('#user-recent-answers-table').html(response);
+      $('#answer-expand').hide();
+    })
+    .fail(function() {
+      console.log("error");
+    })
+
+
+  });
+
+  $('#profile-page-container').on('click','#comment-expand', function(event){
+    event.preventDefault();
+    console.log($(this).attr('href'));
+
+    $.ajax({
+      url: $(this).attr('href'),
+    })
+
+    .done(function(response) {
+      $('#user-comments-table').html(response);
+      $('#comment-expand').hide();
+    })
+    .fail(function() {
+      console.log("error");
+    })
+
+
+  });
+
+});
