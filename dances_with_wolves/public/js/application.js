6824e50eee8c0608f10dfc6f297f5798771c5999
Audrey Kang <hkang10@gmail.com>
2016-09-22 22:57:22 -0500

    the only thing is not working is the comment for answers on the ajax portion

--- dances_with_wolves/public/js/application.js
+++ dances_with_wolves/public/js/application.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
   $('.question-comment-form').hide();
+  // $('.answer-comment-container').hide();
 
   $('.new-question-comment-form').on('click', function(event){
     event.preventDefault();
@@ -8,19 +9,19 @@ $(document).ready(function() {
     var $button = $(this);
 
     var url = $button.find('form').attr('action');
-    var method = $button.find('form').attr('method');
+    // var method = $button.find('form').attr('method');
 
     var request = $.ajax({
       url: url
     });
 
     request.done(function(response) {
-      $('#post-question-button').hide();
-      $('#new-question-form').html(response);
+      $('#post-question-comment-button').hide();
+      $('#new-question-comment').html(response);
     });
   });
 
-  $('#new-question-form').on('submit', '.question-comment-form', function(event){
+  $('#new-question-comment').on('submit', '.question-comment-form', function(event){
     event.preventDefault();
 
     var $button = $(this);
@@ -37,6 +38,9 @@ $(document).ready(function() {
 
     request.done(function(response){
       $('#question-comment-table').append(response);
+      $button.trigger('reset');
+      $button.hide();
+      $button.parent().closest('div').first().find('#post-question-comment-button').show();
     })
   });
 
@@ -72,9 +76,52 @@ $(document).ready(function() {
 
     request.done(function(response){
       $button.hide();
-      console.log(response)
       $('.answer-list').append(response);
     });
-
   });
+
+
+  // $('#answer-comment-list').on('submit', '.new-answer-comment-form', function(event){
+  //   event.preventDefault();
+  //
+  //   var $button = $(this);
+  //   // console.log($button);
+  //   var url = $button.find('form').attr('action');
+  //
+  //   var request = $.ajax({
+  //     url: url,
+  //     method: "GET"
+  //   });
+  //
+  //   request.done(function(response) {
+  //     $('#post-answer-comment-button').hide();
+  //     $('#new-answer-comment').html(response);
+  //   });
+  // });
+
+  // $('#new-answer-comment').on('submit', '.answer-comment-form', function(event){
+  //   event.preventDefault();
+  //
+  //   var $button = $(this);
+  //   var method = $button.attr('method');
+  //   var url = $button.attr('action');
+  //   var data = $button.serialize();
+  //
+  //   var request = $.ajax({
+  //     method: method,
+  //     url: url,
+  //     data: data
+  //   });
+  //
+  //   request.done(function(response){
+  //     console.log(response);
+      // var article = $button.parent().parent().first().children().first().next.clone();
+      // article.find('td:nth-child(4)').text(response)
+  //     $('#question-comment-table').append(response);
+  //     $button.trigger('reset');
+  //     $button.hide();
+  //     $button.parent().closest('div').first().find('#post-question-comment-button').show();
+    // })
+// });
+
 });
